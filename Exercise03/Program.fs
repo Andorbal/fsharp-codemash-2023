4386de58cc4d6985549b84584df190010ca1c139
Andrew Benz <andorbal@gmail.com>
2023-01-11 10:25:18 -0600

    Add exercise 3

---
+++ Exercise03/Program.fs
@@ -0,0 +1,10 @@
+﻿open FSharp.Data
+
+type Courses = CsvProvider<"./Courses.csv">
+
+Courses.GetSample().Rows
+|> Seq.filter (fun x -> x.CompletionPercentage > 0.99M)
+|> Seq.map (fun x -> $"%s{x.FirstName} %s{x.LastName}")
+|> Seq.distinct
+|> Seq.sort
+|> Seq.iter (printfn "%s")
